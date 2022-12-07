1328366d4253d8f81cae80b0e9cefdb24778e31d
Bob Urberger <burberger@gmail.com>
2022-12-07 01:12:08 -0800

    Finish day 3 part 2

--- AdventOfCode/Rucksack.fs
+++ AdventOfCode/Rucksack.fs
@@ -16,13 +16,22 @@ let getRepeatItem(itemStr: string): char =
 
     (Set.intersect firstHalfItems lastHalfItems).MinimumElement
 
-let getSharedElement(itemsList: seq<string>) =
-    sharedElement
+let getSharedElement(itemsList: seq<string>, index: int): Option<char * int> =
+    if Seq.length itemsList = index then 
+        None
+    else
+        let group = itemsList |> Seq.skip index |> Seq.take 3
+        let sharedChar = group |> Seq.map(fun x -> x |> Set.ofSeq) |> Set.intersectMany |> Set.minElement
+        Some((sharedChar, index + 3))
 
 
 let getDay3Solution =
     printf "\nDAY 3\n"
 
-    let itemsList = readFile @"C:\Users\bob\source\repos\AdventOfCode\AdventOfCode\rucksackItems.txt"
+    let itemsList = readFile @"C:\Users\bob\source\repos\AdventOfCode\AdventOfCode\rucksackItems.txt" |> Seq.toList
     let totalRepeatScore = itemsList |> Seq.map (fun x -> (getItemScore(getRepeatItem x).Value)) |> Seq.sum
+    let groupChars = 0 |> Seq.unfold(fun index -> getSharedElement(itemsList, index))
+    let totalGroupScore = groupChars |> Seq.map (fun x -> (getItemScore x).Value) |> Seq.sum
+
     printf "%d\n" totalRepeatScore
+    printf "%d\n" totalGroupScore
